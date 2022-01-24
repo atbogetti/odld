ac5b71dee4e3223b3ef7a83f345587b21f1aa3d9
Anthony Bogetti <anthony.bogetti@gmail.com>
2022-01-24 11:16:41 -0500

    added option for shorter duration timing calculation in script

--- analysis/get_timings.py
+++ analysis/get_timings.py
@@ -1,9 +1,12 @@
 import h5py
+import sys
+
+last = int(sys.argv[1])
 
 f = h5py.File('west.h5', 'r')['summary']
 
-walltime = f['walltime'].sum()
-cputime = f['cputime'].sum()
+walltime = f['walltime'][0:last].sum()
+cputime = f['cputime'][0:last].sum()
 
 print("walltime: ", walltime, "s")
 print("cputime: ", cputime, "s")
