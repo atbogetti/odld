96df8787cd6de2ea6de42188e7ee9a7d980a05c8
Anthony Bogetti <anthony.bogetti@gmail.com>
2022-03-31 10:05:50 -0400

    cleaned up imports in group.py

--- group.py
+++ group.py
@@ -1,7 +1,6 @@
 import westpa
 import logging
-from sklearn.cluster import KMeans
-from sklearn.cluster import AffinityPropagation
+from sklearn import cluster
 import numpy
 
 log = logging.getLogger(__name__)
@@ -10,9 +9,29 @@ log.debug('loading module %r' % __name__)
 def kmeans(coords, n_clusters, splitting):
     X = numpy.array(coords)
     X = X.reshape(-1,1)
-    km = KMeans(n_clusters=n_clusters).fit(X)   
+    km = cluster.KMeans(n_clusters=n_clusters).fit(X)   
     cluster_centers_indices = km.cluster_centers_
     labels = km.labels_
     if splitting:
         print("cluster centers:", numpy.sort(cluster_centers_indices.flatten()))
+    return labels
+
+def affinity_propagation(coords, n_clusters, splitting):
+    X = numpy.array(coords)
+    X = X.reshape(-1,1)
+    ap = cluster.AffinityPropagation().fit(X)   
+    cluster_centers_indices = ap.cluster_centers_
+    labels = ap.labels_
+    if splitting:
+        print("cluster centers:", numpy.sort(cluster_centers_indices.flatten()))
+    return labels
+
+def dbscan(coords, n_clusters, splitting):
+    X = numpy.array(coords)
+    X = X.reshape(-1,1)
+    db = cluster.DBSCAN().fit(X)   
+    components = db.components_
+    labels = db.labels_
+    if splitting:
+        print("cluster components:", numpy.sort(components.flatten()))
     return labels
