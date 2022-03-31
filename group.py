6094c5ba607c67023105543972f5f2ae65048f3c
Anthony Bogetti <anthony.bogetti@gmail.com>
2022-03-31 09:31:50 -0400

    no cluster centers printed if not splitting

--- group.py
+++ group.py
@@ -7,11 +7,12 @@ import numpy
 log = logging.getLogger(__name__)
 log.debug('loading module %r' % __name__)
 
-def kmeans(coords, n_clusters, **kwargs):
+def kmeans(coords, n_clusters, splitting):
     X = numpy.array(coords)
     X = X.reshape(-1,1)
     km = KMeans(n_clusters=n_clusters).fit(X)   
     cluster_centers_indices = km.cluster_centers_
     labels = km.labels_
-    print("cluster centers:", cluster_centers_indices.flatten())
+    if splitting:
+        print("cluster centers:", numpy.sort(cluster_centers_indices.flatten()))
     return labels
